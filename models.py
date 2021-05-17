a4cb66c8d6dc53a4138bd03bf2597be573a34160
Nathan Mainka Soares <nathan.soares.ns@gmail.com>
2021-05-16 23:30:14 -0300

    fixed Cat class and BaseM

--- models.py
+++ models.py
@@ -1,14 +1,15 @@
 from typing import List, Optional
 from fastapi import FastAPI, Body
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
-    tags: List[str] = []
+class Cat(BaseModel):
+    name: str = Field(..., max_length=50, description="Cat's name")
+    breed: str = Field(..., max_length=300,description="Cat's breed")
+    location_of_origin: str = Field(..., max_length=150,description="Cat's location of origin")
+    coat_length: float = Field(..., gt=0, description="Cat's coat length")
+    body_type: str = Field(..., max_length=50, description="Cat's body type")
+    pattern: str = Field(..., max_length=50, description="Cat's pattern")
 
 class BaseM(BaseModel):
-    list_of_cats: List[Item] = Body(...)
+    list_of_cats: List[Cat] = Body(...)
